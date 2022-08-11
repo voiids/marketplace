225a83488763cd99bda87ab0f54ce51ce8d87fe9
VoiiDz <moncifjb@gmail.com>
2022-08-11 23:16:46 +0100

    add config with rollup

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface ButtonProps {
+    label: string;
+}
+
+const Button = (props: ButtonProps) => {
+    return (
+        <button style={{ border: "none", padding: "10px 20px", backgroundColor: "lightblue" }}>{props.label}</button>
+    );
+};
+
+export default Button;
