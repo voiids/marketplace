dae589461c245977a83ba0e4b95e62ea9b0d2650
VoiiDz <moncifjb@gmail.com>
2022-08-11 19:15:59 +0100

    add btn

---
+++ src/SobBtnTest.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SobBtnTest = () => {
+    return <button style={{ border: "none", padding: "10px 20px", backgroundColor: "lightblue" }}>SobBtnTest</button>;
+};
+
+export default SobBtnTest;
