6a507dd81d136575a6fbfd9db62d55afeb11be61
VoiiDz <moncifjb@gmail.com>
2022-08-14 01:30:58 +0100

    add tests, storybook and sass

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -20,11 +23,29 @@ export default [
                 sourcemap: true,
             },
         ],
-        plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({
+                exclude: [
+                    // Exclude test files
+                    /\.test.((js|jsx|ts|tsx))$/,
+                    // Exclude story files
+                    /\.stories.((js|jsx|ts|tsx|mdx))$/,
+                ],
+                tsconfig: "./tsconfig.json",
+            }),
+            postcss({
+                minimize: true,
+            }),
+            terser(),
+        ],
     },
     {
         input: "dist/esm/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+        external: [/\.(css|less|scss)$/, "react", "react-dom"],
     },
 ];
